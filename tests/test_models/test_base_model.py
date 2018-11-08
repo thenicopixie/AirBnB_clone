d91bebd86d8dc537e962181c064cf93bdbce8429
JackWanaCode <kazucuong@yahoo.com>
2018-11-08 21:56:01 +0000

    first commit

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Unittest for Base Model"""
+
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBase(unittest.TestCase):
+    """ Test for Base Model
+    """
+    def test_base_00(self):
+        """Test for first instance
+        """
+        my_model = BaseModel()
+        my_model.name = "Holberton"
+        my_model.my_number = 89
+        self.assertEqual(my_model.name, "Holberton")
+        self.assertEqual(my_model.my_number, 89)
+        self.assertEqual(type(my_model.created_at), str)
+        self.assertEqual(type(my_model.updated_at), str)
+        self.assertEqual(type(my_model.id), str)
+        my_model_json = my_model.to_dict()
+        self.assertTrue(type(my_model_json), dict)
+        self.assertTrue(my_model_json.keys(), [['id'], ['created_at'],
+                                                    ['my_number'], ['updated_at'],
+                                                    ['name']])
+
+if __name__ == '__main__':
+    unittest.main()
