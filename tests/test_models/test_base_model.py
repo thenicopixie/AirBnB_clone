faf32e131fe7c3eeead6ce1eeb70b998b9d601c9
JackWanaCode <kazucuong@yahoo.com>
2018-11-08 22:28:33 +0000

    change test string of datetime object

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -16,10 +16,10 @@ class TestBase(unittest.TestCase):
         my_model.my_number = 89
         self.assertEqual(my_model.name, "Holberton")
         self.assertEqual(my_model.my_number, 89)
-        self.assertEqual(type(my_model.created_at), str)
-        self.assertEqual(type(my_model.updated_at), str)
         self.assertEqual(type(my_model.id), str)
         my_model_json = my_model.to_dict()
+        self.assertEqual(type(my_model_json['created_at']), str)
+        self.assertEqual(type(my_model_json['updated_at']), str)
         self.assertTrue(type(my_model_json), dict)
         self.assertTrue(my_model_json.keys(), [['id'], ['created_at'],
                                                     ['my_number'], ['updated_at'],
