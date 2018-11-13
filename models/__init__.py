f29f3d5e9ea3fe37b3247938c462e8f639a1b2c6
JackWanaCode <kazucuong@yahoo.com>
2018-11-13 20:25:00 +0000

    formatting the content

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ FileStorage instance, for saving to storage
 """
-from .engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
+
 storage = FileStorage()
 storage.reload()
