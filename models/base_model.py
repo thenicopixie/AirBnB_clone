3ca12db9b6c76f51ae02d6909d00a1ec9cfef184
thenicopixie <391@holbertonschool.com>
2018-11-13 19:24:52 +0000

    fix pep8 styling errors

--- models/base_model.py
+++ models/base_model.py
@@ -15,11 +15,10 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-	#if new instantce is not in _file storage objects, make new one
+        # if new instantce is not in _file storage objects, make new one
         if kwargs == {}:
-        #if not hasattr(storage.__object, storage):
             models.storage.new(self)
-	#copy a new dictionary of attribute for new instance
+        # copy a new dictionary of attribute for new instance
         elif kwargs != {} and kwargs['__class__'] == self.__class__.__name__:
             for key, value in kwargs.items():
                 if key == "created_at":
@@ -41,7 +40,7 @@ class BaseModel:
         """ update the with the current datetime
         """
         self.updated_at = datetime.now()
-        #save new objects to File json
+        # save new objects to File json
         models.storage.save()
 
     def to_dict(self):
