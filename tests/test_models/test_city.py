3aec7dacb77795cfd06bc28265da0feb16c86da8
JackWanaCode <kazucuong@yahoo.com>
2018-11-14 07:38:46 +0000

    add content to test

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -3,11 +3,43 @@
 """
 
 import unittest
+from models.city import City
+import os
+import json
 
 
 class TestCity(unittest.TestCase):
     """ Test User class
     """
+    name = "file.json"
+
+    def test_class_attributes(self):
+        """ Test User class attributes """
+        check = 0
+        my_city = City()
+        """
+        Test keys in User dictionary
+        """
+        self.assertTrue(sorted(list(my_city.__dict__.keys())) ==
+                        ['created_at', 'id', 'updated_at'], True)
+        """
+        Test for class attributes in User
+        """
+        my_city.name = "Vallejo"
+        my_city.save()
+        self.assertEqual(my_city.name, "Vallejo")
+        """
+        Test file.json store the object just created
+        """
+        if os.path.isfile(TestCity.name):
+            with open(TestCity.name, 'r') as f:
+                string = f.read()
+                dic = json.loads(string)
+                for key, value in dic.items():
+                    if key.split('.')[1] == my_city.id:
+                        check = 1
+                        break
+        self.assertEqual(check, 1)
 
 if __name__ == '__main__':
     unittest.main()
