5acd28e1af873cb74d7f16ce63c5fbaa4062fac8
JackWanaCode <kazucuong@yahoo.com>
2018-11-14 22:07:16 +0000

    add setup n teardown

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,9 +4,20 @@
 
 import unittest
 from models.user import User
+from models import storage
 import os
 import json
 
+
+def setUpModule():
+    if os.path.isfile("file.json"):
+        os.remove("file.json")
+    storage._FileStorage__objects.clear()
+
+
+def tearDownModule():
+    if os.path.isfile("file.json"):
+        os.remove("file.json")
 
 class TestUser(unittest.TestCase):
     """ Test User class
